aa031e2284b35adb5389fcf03d9d8dde660e7b6a
Developer <developer@example.com>
2026-08-17 21:54:07 -0700

    Fix missing WebSettings import

--- app/src/main/java/com/webworkspace/app/ui/WorkspaceActivity.kt
+++ app/src/main/java/com/webworkspace/app/ui/WorkspaceActivity.kt
@@ -14,6 +14,7 @@ import android.webkit.WebResourceError
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.webkit.WebSettings
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.ImageButton
